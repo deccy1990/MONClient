cc0d51807c229ac544083e0438a19dfba1f2be91
deccy1990 <declancronin@live.com>
2026-01-24 18:02:36 +0000

    Working tile and map rendering correctly

---
+++ assets/maps/Overworld - Forest - Thick 256x144.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Overworld - Forest - Thick 256x144" tilewidth="64" tileheight="32" tilecount="324" columns="12">
+ <image source="../../../Users/decla/Downloads/SBS - Isometric Overworld Pack - Large/Overworld - Large/Thick/Overworld - Forest - Thick 256x144.png" width="768" height="864"/>
+</tileset>
