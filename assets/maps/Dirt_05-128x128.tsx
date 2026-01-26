1b20ee5ac7a392deb6d6fe7514fdc4513aa44c26
Declan Cronin <declancronin@live.com>
2026-01-26 11:54:58 +0000

    Assets

---
+++ assets/maps/Dirt_05-128x128.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Dirt_05-128x128" tilewidth="64" tileheight="32" tilecount="8" columns="2">
+ <image source="../../../MONASSETSNOTUSED/SBS_-_Tiny_Texture_Pack_2_-_128x128/128x128/Dirt/Dirt_05-128x128.png" width="128" height="128"/>
+</tileset>
---
+++ assets/maps/Overworld - Water - Flat 256x128.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Overworld - Water - Flat 256x128" tilewidth="64" tileheight="32" tilecount="288" columns="12">
+ <image source="../../../MONASSETSNOTUSED/SBS - Isometric Overworld Pack - Large/Overworld - Large/Flat/Overworld - Water - Flat 256x128.png" trans="ff00ff" width="768" height="768"/>
+</tileset>
