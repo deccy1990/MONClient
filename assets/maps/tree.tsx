cec9b377c525cb8ce332add1c8e13c8b593f88b8
Declan Cronin <declancronin@live.com>
2026-01-29 11:49:26 +0000

    Asset update

---
+++ assets/maps/tree.tsx
@@ -0,0 +1,76 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="tree" tilewidth="256" tileheight="256" tilecount="24" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="../Tiles/Autumn/256x256/Tree01_0001.png" width="256" height="256"/>
+ </tile>
+ <tile id="1">
+  <image source="../Tiles/Autumn/256x256/Tree01_0002.png" width="256" height="256"/>
+ </tile>
+ <tile id="2">
+  <image source="../Tiles/Autumn/256x256/Tree01_0003.png" width="256" height="256"/>
+ </tile>
+ <tile id="3">
+  <image source="../Tiles/Autumn/256x256/Tree01_0004.png" width="256" height="256"/>
+ </tile>
+ <tile id="4">
+  <image source="../Tiles/Autumn/256x256/Tree01_0005.png" width="256" height="256"/>
+ </tile>
+ <tile id="5">
+  <image source="../Tiles/Autumn/256x256/Tree01_0006.png" width="256" height="256"/>
+ </tile>
+ <tile id="6">
+  <image source="../Tiles/Autumn/256x256/Tree01_0007.png" width="256" height="256"/>
+ </tile>
+ <tile id="7">
+  <image source="../Tiles/Autumn/256x256/Tree01_0008.png" width="256" height="256"/>
+ </tile>
+ <tile id="8">
+  <image source="../Tiles/Autumn/256x256/Tree01_0009.png" width="256" height="256"/>
+ </tile>
+ <tile id="9">
+  <image source="../Tiles/Autumn/256x256/Tree01_0010.png" width="256" height="256"/>
+ </tile>
+ <tile id="10">
+  <image source="../Tiles/Autumn/256x256/Tree01_0011.png" width="256" height="256"/>
+ </tile>
+ <tile id="11">
+  <image source="../Tiles/Autumn/256x256/Tree01_0012.png" width="256" height="256"/>
+ </tile>
+ <tile id="12">
+  <image source="../Tiles/Autumn/256x256/Tree01_0013.png" width="256" height="256"/>
+ </tile>
+ <tile id="13">
+  <image source="../Tiles/Autumn/256x256/Tree01_0014.png" width="256" height="256"/>
+ </tile>
+ <tile id="14">
+  <image source="../Tiles/Autumn/256x256/Tree01_0015.png" width="256" height="256"/>
+ </tile>
+ <tile id="15">
+  <image source="../Tiles/Autumn/256x256/Tree01_0016.png" width="256" height="256"/>
+ </tile>
+ <tile id="16">
+  <image source="../Tiles/Autumn/256x256/Tree01_0017.png" width="256" height="256"/>
+ </tile>
+ <tile id="17">
+  <image source="../Tiles/Autumn/256x256/Tree01_0018.png" width="256" height="256"/>
+ </tile>
+ <tile id="18">
+  <image source="../Tiles/Autumn/256x256/Tree01_0019.png" width="256" height="256"/>
+ </tile>
+ <tile id="19">
+  <image source="../Tiles/Autumn/256x256/Tree01_0020.png" width="256" height="256"/>
+ </tile>
+ <tile id="20">
+  <image source="../Tiles/Autumn/256x256/Tree01_0021.png" width="256" height="256"/>
+ </tile>
+ <tile id="21">
+  <image source="../Tiles/Autumn/256x256/Tree01_0022.png" width="256" height="256"/>
+ </tile>
+ <tile id="22">
+  <image source="../Tiles/Autumn/256x256/Tree01_0023.png" width="256" height="256"/>
+ </tile>
+ <tile id="23">
+  <image source="../Tiles/Autumn/256x256/Tree01_0024.png" width="256" height="256"/>
+ </tile>
+</tileset>
